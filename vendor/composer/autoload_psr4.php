8a399ad397a6d5c3594dbabc5c3921bd20eff968
root <root@ubunu2004.linuxvmimagrs.local>
2021-05-17 23:19:25 +0530

    new version

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -45,6 +45,7 @@ return array(
     'Psr\\Log\\' => array($vendorDir . '/psr/log/Psr/Log'),
     'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-message/src'),
     'Psr\\Container\\' => array($vendorDir . '/psr/container/src'),
+    'Psr\\Cache\\' => array($vendorDir . '/psr/cache/src'),
     'PhpParser\\' => array($vendorDir . '/nikic/php-parser/lib/PhpParser'),
     'OomphInc\\ComposerInstallersExtender\\' => array($vendorDir . '/oomphinc/composer-installers-extender/src'),
     'Masterminds\\' => array($vendorDir . '/masterminds/html5/src'),
